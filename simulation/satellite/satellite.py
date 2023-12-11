6570f26cfc0f4d73e6eafbd55cb6ca82dd2cb2ab
Tobias Klockau <tobias.klockau@deutschebahn.com>
2023-12-11 23:05:29 +0100

    abstracted state calculations

--- simulation/satellite/satellite.py
+++ simulation/satellite/satellite.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
-import numpy as np
 
 from .satellite_state import SatelliteState
 from .satellite_properties import SatelliteProperties
 from .components.reaction_wheel import ReactionWheel
 from ..utils.make_fields_classvariables import make_fields_classvariables
-from ..utils.quantities import AngularVelocity, MomentOfInertia, Time
+from ..utils.quantities import AngularVelocity, Time
 
 @dataclass
 class Satellite:
@@ -13,21 +12,21 @@ class Satellite:
     properties: SatelliteProperties
     reaction_wheel: ReactionWheel
 
-    def propagate(self, reaction_wheel_angular_velocity: AngularVelocity, time_difference: Time) -> "Satellite":
+    def propagate(self, reaction_wheel_angular_velocity: AngularVelocity, time_delta: Time) -> "Satellite":
 
-        new_angular_velocity = - (self.reaction_wheel.moment_of_inertia / self.properties.moment_of_inertia) * reaction_wheel_angular_velocity
-        new_attitude = self.state.attitude + new_angular_velocity * time_difference
+        reaction_wheel = ReactionWheel(
+            reaction_wheel_angular_velocity, 
+            self.reaction_wheel.moment_of_inertia
+        )
 
         return Satellite(
-            state=SatelliteState(
-                attitude=new_attitude,
-                angular_velocity=new_angular_velocity
+            state=self.state.propagate(
+                self.properties,
+                reaction_wheel=reaction_wheel,
+                time_delta=time_delta
             ),
             properties=self.properties,
-            reaction_wheel=ReactionWheel(
-                angular_velocity=reaction_wheel_angular_velocity,
-                moment_of_inertia=self.reaction_wheel.moment_of_inertia
-            )
+            reaction_wheel=reaction_wheel,
         )
 
 make_fields_classvariables(Satellite)
--- simulation/satellite/satellite_state.py
+++ simulation/satellite/satellite_state.py
@@ -1,11 +1,27 @@
 from dataclasses import dataclass
 
 from ..utils.make_fields_classvariables import make_fields_classvariables
-from ..utils.quantities import Angle, AngularVelocity
+from ..utils.quantities import Angle, AngularVelocity, Time
+from .components.reaction_wheel import ReactionWheel
+from .satellite_properties import SatelliteProperties
 
 @dataclass
 class SatelliteState:
     attitude: Angle
     angular_velocity: AngularVelocity
+
+    def propagate(self, properties: SatelliteProperties, reaction_wheel: ReactionWheel, time_delta: Time) -> "SatelliteState":
+        return SatelliteState(
+            attitude=self._calc_attitude(properties, reaction_wheel, time_delta),
+            angular_velocity=self._calc_angular_velocity(properties, reaction_wheel)
+        )
+
+    def _calc_attitude(self, properties: SatelliteProperties, reaction_wheel: ReactionWheel, time_delta: Time) -> Angle:
+        angular_velocity = self._calc_angular_velocity(properties, reaction_wheel)
+        return self.attitude + angular_velocity * time_delta
+
+    def _calc_angular_velocity(self, properties: SatelliteProperties, reaction_wheel: ReactionWheel) -> AngularVelocity:
+        moment_of_inertia_ratio = reaction_wheel.moment_of_inertia / properties.moment_of_inertia
+        return - moment_of_inertia_ratio * reaction_wheel.angular_velocity
 
 make_fields_classvariables(SatelliteState, ".state")
